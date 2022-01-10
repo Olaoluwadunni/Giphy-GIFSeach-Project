4d7ed2ddc78404ff0e1749ed31e32b29953f6739
oluwatimileyin <oluwatimileyinakinpelu49@gmail.com>
2022-01-10 15:41:43 +0100

    changes

---
+++ src/helpers/variables.js
@@ -0,0 +1,5 @@
+const apiKey = process.env.REACT_APP_GIPHY_API_KEY
+const baseUrl = 'https://api.giphy.com/v1/gifs/search'	
+console.log(baseUrl);
+console.log(apiKey)
+export {baseUrl, apiKey}
---
+++ src/services/getGIF.js
@@ -0,0 +1,10 @@
+import { apiKey, baseUrl } from "../helpers/variables"
+import axios from 'axios'
+
+const getGIF = async(category) => {
+    const url = `${baseUrl}?api_key=${apiKey}&q=${category}&limit=25&offset=0&rating=g&lang=en`;
+        return  axios.get(url)
+        }
+
+
+export default getGIF
