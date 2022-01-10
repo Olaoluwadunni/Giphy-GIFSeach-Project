9773bab393d468a7f18854a499df20bd6735a609
oluwatimileyin <oluwatimileyinakinpelu49@gmail.com>
2022-01-10 15:41:20 +0100

    API integration

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import getGIF from './services/getGIF';
+
 
 function App() {
+  const [searchInput, setSearchInput] = useState()
+  const [gif, setGif] = useState([])
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    // console.log(searchInput)
+    getGIF(searchInput)
+    .then((response) => setGif(response?.data))
+    .catch((err) => console.log(JSON.stringify(err))); 
+  }
+  if (gif.length = 0) {
+    return (
+      <div>
+        This is it
+      </div>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App mt-4">
+      <div className='d-flex justify-content-center align-center mx-auto'>
+      {/* {searchInput} */}
+        <form onSubmit={handleSubmit} className='d-flex w-50'>
+          <input 
+            className="form-control me-2" 
+            list="datalistOptions" 
+            id="exampleDataList" 
+            placeholder="Type to search..."
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
+          <input 
+            className="btn btn-primary" 
+            type="submit" 
+            value="Search"
+          />
+        </form>
+      </div>
+      <hr />
+      GIF:
+      <br />
+      {gif.data ? gif?.data?.map((each) => 
+      {
+        const {images} = each
+      return (
+        <>
+          <img 
+            width="300em"
+            height="250em"
+            src={images.downsized.url} 
+            alt={images.downsized.url}
+            className='me-4 mt-4'
+          />
+        </>
+        )
+      })
+    : 'Nothing to display here. Please search for something'}
     </div>
   );
 }
